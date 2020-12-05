b0e0ad4800372b186b2a46753cd5a62a2a48024d
kicok <kicoker@gmail.com>
2020-12-05 16:18:40 +0900

    first commit

---
+++ flutter_weather_bloc6/android/app/src/main/kotlin/com/example/flutter_weather_bloc6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_weather_bloc6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_weather_bloc6/lib/pages/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'search_page.dart';
+import 'setting_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SettingsPage();
+                  },
+                ),
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SearchPage();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Weather',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
