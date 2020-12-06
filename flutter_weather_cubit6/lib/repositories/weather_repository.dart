96b146c63649a3ac1909fbedb7920927badc20f1
kicok <kicoker@gmail.com>
2020-12-06 22:29:16 +0900

    update Cubit - change path in weather_repository.dart

--- flutter_weather_cubit6/lib/repositories/weather_repository.dart
+++ flutter_weather_cubit6/lib/repositories/weather_repository.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter_weather_cubit6/models/weather.dart';
+
+import '../models/weather.dart';
 import 'weather_api_client.dart';
 
 class WeatherRepository {
